1649461bdbad4f6898a716dcf84b48f98923d68a
Stephen Eilert <contact@stepheneilert.com>
2015-09-08 11:22:11 -0300

    Find file uses ido instead of helm.

--- src/emacs-d/customizations/01-features.el
+++ src/emacs-d/customizations/01-features.el
@@ -7,7 +7,7 @@
 (setq helm-buffers-fuzzy-matching t
       helm-recentf-fuzzy-match    t)
 
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;;(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (helm-autoresize-mode t)
 ;(global-set-key (kbd "C-c h") 'helm-command-prefix)
@@ -23,7 +23,9 @@
       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-      helm-ff-file-name-history-use-recentf t)
+      helm-ff-file-name-history-use-recentf t
+      helm-candidate-number-limit           30)
+
 
 (helm-mode 1)
 
